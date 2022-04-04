31534cce67201a49a848e21a46a02c84b1f49cdf
xhguleixin123 <18017158729@163.com>
2022-04-04 23:58:30 +0800

    🎈 perf: 重组织文件结构

---
+++ src/components/ThreeComponent.js
@@ -0,0 +1,78 @@
+
+import React, { Component } from 'react';
+import * as THREE from 'three';
+
+class ThreeComponent extends Component {
+    constructor(props) {
+        super(props);
+        this.m_Renderer = new THREE.WebGLRenderer({
+            antialias: true
+        });
+        this.scene = new THREE.Scene();
+        this.camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 10000);
+        this.camera.position.x = 0;
+        this.camera.position.y = 0;
+        this.camera.position.z = 2;
+        this.state = {
+
+        };
+    }
+
+    initThree() {
+
+        var width, height;
+        width = window.innerWidth;
+        height = window.innerHeight;
+        this.m_Renderer.setSize(width, height);
+        document.getElementById('canvas-frame').appendChild(this.m_Renderer.domElement);
+        this.m_Renderer.setClearColor(0x000000, 1.0);
+
+
+        var light;
+        light = new THREE.AmbientLight(0xFFFFFF);
+        light.position.set(300, 300, 0);
+        this.scene.add(light);
+
+        var geometry = new THREE.TorusGeometry(.7, .2, 16, 100);
+        var material = new THREE.MeshLambertMaterial({ color: 0xFF0000 });
+        var mesh = new THREE.Mesh(geometry, material);
+        mesh.position.set(0, 0, 0);
+        this.scene.add(mesh);
+
+        const clock = new THREE.Clock();
+        const tick = () => {
+
+            const elapsedTime = clock.getElapsedTime()
+        
+            // // Update objects
+            // sphere.rotation.y = .5 * elapsedTime
+        
+            // // Update Orbital Controls
+            // controls.update()
+        
+            // Render
+            this.m_Renderer.render(this.scene, this.camera)
+        
+            // Call tick again on the next frame
+            window.requestAnimationFrame(tick)
+        }
+        tick();
+    }
+
+    /**
+     * 开始Three
+     *
+     * @memberof ThreeComponent
+     */
+    componentDidMount() {
+        this.initThree();
+    }
+    render() {
+        return (
+            <div id='canvas-frame'>
+            </div>
+        );
+    }
+}
+
+export default ThreeComponent;
